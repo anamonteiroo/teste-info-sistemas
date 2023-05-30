f4092de6f1d8bcca40311931c90dc1966b98f2b0
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-30 16:10:07 -0300

    feat: create route read veiculos

--- src/api/app.js
+++ src/api/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
+app.use(bodyParser.json());
 
-app.use(express.urlencoded({ extended: true }));
+const dataFilePath = 'src/models/veiculos.json';
 
-app.use(express.json());
-
-app.get('/carros', async (req, res) => {
-    return console.log('Hello, world!');
-});
+app.get('/veiculos', (req, res) => {
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Erro ao ler os veículos.' });
+      }
+  
+      try {
+        const veiculos = JSON.parse(data);
+        res.json(veiculos);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao processar os veículos.' });
+      }
+    });
+  });
 
 module.exports = app

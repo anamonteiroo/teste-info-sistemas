459e1b99154922fcbf5c2daa859d71d1d7240548
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-30 17:29:49 -0300

    feat: route create veiculos

--- src/api/app.js
+++ src/api/app.js
@@ -49,4 +49,32 @@ app.get('/veiculos/:id', (req, res) => {
     });
 });
 
+app.post('/veiculos', (req, res) => {
+    const newVeiculo = req.body;
+
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
+        }
+
+        try {
+            const veiculos = JSON.parse(data);
+            veiculos.push(newVeiculo);
+
+            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao salvar o veículo.' });
+                }
+
+                res.status(201).json(newVeiculo);
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao processar os veículos.' });
+        }
+    });
+});
+
 module.exports = app

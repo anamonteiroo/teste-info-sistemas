1b32bcaad74aae84a3b39fb2f74eb8a420de6734
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-30 13:54:23 -0300

    feat: conexão express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.get('/carros', async (req, res) => {
+    return console.log('Hello, world!');
+});
+
+module.exports = app

444bc197299356c4be80166a3ec63f1a57e6868b
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-30 18:07:14 -0300

    feat: route delete create

--- src/api/app.js
+++ src/api/app.js
@@ -80,35 +80,70 @@ app.post('/veiculos', (req, res) => {
 app.put('/veiculos/:id', (req, res) => {
     const veiculoId = req.params.id;
     const updatedVeiculo = req.body;
-  
+
     fs.readFile(dataFilePath, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Erro ao ler os veículos.' });
-      }
-  
-      try {
-        const veiculos = JSON.parse(data);
-        const index = veiculos.findIndex(v => v.id === veiculoId);
-  
-        if (index === -1) {
-          return res.status(404).json({ error: 'Veículo não encontrado.' });
-        }
-  
-        veiculos[index] = { ...veiculos[index], ...updatedVeiculo };
-  
-        fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
-          if (err) {
+        if (err) {
             console.error(err);
-            return res.status(500).json({ error: 'Erro ao atualizar o veículo.' });
-          }
-  
-          res.json(veiculos[index]);
-        });
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Erro ao processar os veículos.' });
-      }
+            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
+        }
+
+        try {
+            const veiculos = JSON.parse(data);
+            const index = veiculos.findIndex(v => v.id === veiculoId);
+
+            if (index === -1) {
+                return res.status(404).json({ error: 'Veículo não encontrado.' });
+            }
+
+            veiculos[index] = { ...veiculos[index], ...updatedVeiculo };
+
+            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao atualizar o veículo.' });
+                }
+
+                res.json(veiculos[index]);
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao processar os veículos.' });
+        }
     });
 });
+
+app.delete('/veiculos/:id', (req, res) => {
+    const veiculoId = req.params.id;
+
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
+        }
+
+        try {
+            const veiculos = JSON.parse(data);
+            const index = veiculos.findIndex(v => v.id === veiculoId);
+
+            if (index === -1) {
+                return res.status(404).json({ error: 'Veículo não encontrado.' });
+            }
+
+            const deletedVeiculo = veiculos.splice(index, 1)[0];
+
+            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao excluir o veículo.' });
+                }
+
+                res.json({ "message": "Veículo deletado" });
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao processar os veículos.' });
+        }
+    });
+});
+
 module.exports = app

8cad4ac1d8be9a504350ea1f07d6939ae2e36697
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-31 14:21:50 -0300

    refact: route put refatoring

--- src/api/app.js
+++ src/api/app.js
@@ -10,41 +10,6 @@ const dataFilePath = 'src/models/veiculos.json';
 
 app.use('/veiculos', routes);
 
-app.put('/veiculos/:id', (req, res) => {
-    const veiculoId = req.params.id;
-    const updatedVeiculo = req.body;
-
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
-        }
-
-        try {
-            const veiculos = JSON.parse(data);
-            const index = veiculos.findIndex(v => v.id === veiculoId);
-
-            if (index === -1) {
-                return res.status(404).json({ error: 'Veículo não encontrado.' });
-            }
-
-            veiculos[index] = { ...veiculos[index], ...updatedVeiculo };
-
-            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).json({ error: 'Erro ao atualizar o veículo.' });
-                }
-
-                res.json(veiculos[index]);
-            });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Erro ao processar os veículos.' });
-        }
-    });
-});
-
 app.delete('/veiculos/:id', (req, res) => {
     const veiculoId = req.params.id;
 

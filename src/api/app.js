15c4075a7394d19655b9c81cf9c39bd5ae8490aa
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-31 14:34:27 -0300

    refact: route delete refatoring

--- src/api/app.js
+++ src/api/app.js
@@ -1,47 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const routes = require('../routes/routes');
 
 const app = express();
 app.use(bodyParser.json());
 
-const dataFilePath = 'src/models/veiculos.json';
-
 app.use('/veiculos', routes);
-
-app.delete('/veiculos/:id', (req, res) => {
-    const veiculoId = req.params.id;
-
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
-        }
-
-        try {
-            const veiculos = JSON.parse(data);
-            const index = veiculos.findIndex(v => v.id === veiculoId);
-
-            if (index === -1) {
-                return res.status(404).json({ error: 'Veículo não encontrado.' });
-            }
-
-            const deletedVeiculo = veiculos.splice(index, 1)[0];
-
-            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).json({ error: 'Erro ao excluir o veículo.' });
-                }
-
-                res.json({ "message": "Veículo deletado" });
-            });
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Erro ao processar os veículos.' });
-        }
-    });
-});
 
 module.exports = app
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const router = express.Router();
@@ -105,6 +104,40 @@ router.put('/:id', (req, res) => {
                 }
 
                 res.json(veiculos[index]);
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao processar os veículos.' });
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const veiculoId = req.params.id;
+
+    fs.readFile(dataFilePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
+        }
+
+        try {
+            const veiculos = JSON.parse(data);
+            const index = veiculos.findIndex(v => v.id === veiculoId);
+
+            if (index === -1) {
+                return res.status(404).json({ error: 'Veículo não encontrado.' });
+            }
+
+            const deletedVeiculo = veiculos.splice(index, 1)[0];
+
+            fs.writeFile(dataFilePath, JSON.stringify(veiculos), err => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Erro ao excluir o veículo.' });
+                }
+
+                res.json({ "message": "Veículo deletado" });
             });
         } catch (err) {
             console.error(err);

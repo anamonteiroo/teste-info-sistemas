8698a7bfd45a3335b3eb82d848fcb760a75cf2da
Ana Monteiro <ana.luiza.monteiro@dasa.com.br>
2023-05-31 15:43:44 -0300

    refact: controller getAll created

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require('fs');
+const { getAll } = require('../controllers/getAll');
 const { get } = require('../controllers/get');
 
 const router = express.Router();
@@ -7,38 +8,21 @@ const router = express.Router();
 const dataFilePath = 'src/models/veiculos.json';
 
 router.get('/', (req, res) => {
-
-        try {
-            get(req, res);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Erro ao processar os veículos.' });
-        }
-    });
+    try {
+        getAll(req, res);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao processar os veículos.' });
+    }
+});
 
 router.get('/:id', (req, res) => {
-    const veiculoId = req.params.id;
-
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Erro ao ler os veículos.' });
-        }
-
-        try {
-            const veiculos = JSON.parse(data);
-            const veiculo = veiculos.find(v => v.id === veiculoId);
-
-            if (!veiculo) {
-                return res.status(404).json({ error: 'Veículo não encontrado.' });
-            }
-
-            res.json(veiculo);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ error: 'Erro ao processar os veículos.' });
-        }
-    });
+    try {
+        get(req, res);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao processar os veículos.' });
+    }
 });
 
 router.post('/', (req, res) => {
